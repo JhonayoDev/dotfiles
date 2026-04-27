69c817e6692563a3729b79d8ad1c1df94eab7c11
jhonayo <j.iramirezpavez@gmail.com>
2026-04-27 03:31:38 -0400

    fix: trackpad toggle ID 7->11 para Ubuntu

--- qtile/.config/qtile/scripts/trackpad_toggle.sh
+++ qtile/.config/qtile/scripts/trackpad_toggle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Toggle trackpad bcm5974
-STATUS=$(xinput list-props 7 | grep "Device Enabled" | awk '{print $4}')
+STATUS=$(xinput list-props 11 | grep "Device Enabled" | awk '{print $4}')
 
 if [ "$STATUS" = "1" ]; then
-    xinput disable 7
+    xinput disable 11
     notify-send "Trackpad" "Desactivado" -t 2000
 else
-    xinput enable 7
+    xinput enable 11
     notify-send "Trackpad" "Activado" -t 2000
 fi
 
