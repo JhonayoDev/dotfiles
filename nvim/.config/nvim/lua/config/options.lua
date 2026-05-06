024f36e77ee0369bb309bb303da99b8ec4764382
jhonayo <j.iramirezpavez@gmail.com>
2026-05-06 04:40:56 -0400

    fix(clipboard): cambio de forma de obtener el clipboard para diferentes entornos

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Clipboard: OSC52 en SSH, xclip en local
-if os.getenv("SSH_TTY") or os.getenv("SSH_CLIENT") then
+-- options.lua
+if not os.getenv("DISPLAY") and not os.getenv("WAYLAND_DISPLAY") then
+  -- Sin display: usar OSC 52 (SSH, devcontainer, tmux remoto)
   vim.g.clipboard = {
     name = "OSC 52",
     copy = {
@@ -16,5 +17,6 @@ if os.getenv("SSH_TTY") or os.getenv("SSH_CLIENT") then
     },
   }
 else
+  -- Con display: usar clipboard del sistema
   vim.opt.clipboard = "unnamedplus"
 end
