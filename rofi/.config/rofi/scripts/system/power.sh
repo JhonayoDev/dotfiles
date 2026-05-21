c48e328e7068a9bed78a0515e0a9eecdece029ae
jhonayo <j.iramirezpavez@gmail.com>
2026-05-21 03:38:50 -0400

    feat(rofi) update rofi configuracion para sitema de gestion de bloqueo

--- rofi/.config/rofi/scripts/system/power.sh
+++ rofi/.config/rofi/scripts/system/power.sh
@@ -5,23 +5,23 @@ SCRIPTS="$HOME/.config/rofi/scripts"
 THEME="$HOME/.config/rofi/themes/power-menu.rasi"
 
 options="Back\0icon\x1fgo-previous-symbolic
+Bloquear\0icon\x1fsystem-lock-screen-symbolic
 Apagar\0icon\x1fsystem-shutdown-symbolic
 Reiniciar\0icon\x1fsystem-reboot-symbolic
 Cerrar sesión\0icon\x1fsystem-log-out-symbolic"
 
-chosen=$(
-  echo -e "$options" | rofi \
-    -dmenu \
-    -i \
-    -p "󰐥 Power Menu" \
-    -theme "$THEME"
-)
+chosen=$(echo -e "$options" | rofi \
+  -dmenu \
+  -i \
+  -p "󰐥 Power Menu" \
+  -theme "$THEME")
 
 [ -z "$chosen" ] && exit 0
 
 case "$chosen" in
 *"Back"*) "$SCRIPTS/control_center.sh" ;;
+*"Bloquear"*) loginctl lock-session ;;
 *"Apagar"*) systemctl poweroff ;;
 *"Reiniciar"*) systemctl reboot ;;
-*"Cerrar sesión"*) loginctl terminate-user "$USER" ;;
+*"Cerrar sesión"*) qtile cmd-obj -o cmd -f shutdown ;;
 esac
